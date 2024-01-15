3a4a0ec31bcdaf964c8e1c96c12ab9b184a64191
Mutai05 <kelvok2014@gmail.com>
2024-01-14 18:17:46 -0800

    Review class that inherit from BaseModel

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the Review class."""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Represents a review.
+
+    Attributes:
+        place_id (str): The identifier of the associated place.
+        user_id (str): The identifier of the associated user.
+        text (str): The text content of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
