f3c8e8deb04fabde328272cc0c3b29d449f4d672
Mutai05 <kelvok2014@gmail.com>
2024-01-16 12:26:01 -0800

    Updated user.py file

--- models/user.py
+++ models/user.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
-    """Represents a User.
+    """Class for managing user objects"""
 
-    Attributes:
-        email (str): The email address of the user.
-        password (str): The password associated with the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
-
-    # Default empty strings for user attributes
     email = ""
     password = ""
     first_name = ""
