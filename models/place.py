089f6510d9e0f2ed36143ac51c8ce2f4fea5740a
Mutai05 <kelvok2014@gmail.com>
2024-01-16 12:21:41 -0800

    Modified place.py file

--- models/place.py
+++ models/place.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""This module creates a Place class"""
+
 from models.base_model import BaseModel
 
-class Place(BaseModel):
-    """Represents a place.
 
-    Attributes:
-        city_id (str): The identifier of the associated city.
-        user_id (str): The identifier of the associated user.
-        name (str): The name of the place.
-        description (str): The description of the place.
-        number_rooms (int): The number of rooms in the place.
-        number_bathrooms (int): The number of bathrooms in the place.
-        max_guest (int): The maximum number of guests allowed.
-        price_by_night (int): The nightly price of the place.
-        latitude (float): The latitude coordinate of the place.
-        longitude (float): The longitude coordinate of the place.
-        amenity_ids (list): A list of Amenity identifiers associated with the place.
-    """
+class Place(BaseModel):
+    """Class for managing place objects"""
 
     city_id = ""
     user_id = ""
