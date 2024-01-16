663af38709538eea346cd7b103c695feac3f6b15
Mutai05 <kelvok2014@gmail.com>
2024-01-16 12:16:19 -0800

    Modified amenity class

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Initialization magic method for the 'models' directory."""
+"""Initializes the package"""
 from models.engine.file_storage import FileStorage
-
-# Create an instance of FileStorage for data storage
-data_storage = FileStorage()
-
-# Reload data from storage to populate the instance with existing data
-data_storage.reload()
+storage = FileStorage()
+storage.reload()
