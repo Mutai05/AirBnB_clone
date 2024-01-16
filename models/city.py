92e8a11d3a762ca7216bdb18873eaf76ed150751
Mutai05 <kelvok2014@gmail.com>
2024-01-16 12:20:14 -0800

    Modified city.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
-    """Represents a city.
+    """Class for managing city objects"""
 
-    Attributes:
-        state_id (str): The identifier of the state associated with the city.
-        name (str): The name of the city.
-    """
-
-    # Default empty strings for state_id and city name
     state_id = ""
     name = ""
